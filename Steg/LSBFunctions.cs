388cc20775a3b93e9478ca50d9f9bb392829db12
Nicolas Gonzalez <nicolasnicogonzalez@gmail.com>
2015-12-18 19:48:22 -0500

    Closer to a working readLSB
    ugh

--- Steg/LSBFunctions.cs
+++ Steg/LSBFunctions.cs
@@ -34,7 +34,7 @@ namespace Steg
 
             System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
 
-            for (int i = 2; i < byteMsg.Length; i++)
+            for (int i = 0; i < byteMsg.Length; i++)
             {
                 // If the intended message is different from the preexisting bit, write to it.
                 if ((byteMsg[i] ^ rgbValues[i] % 2) == 1)
@@ -52,49 +52,41 @@ namespace Steg
             bmp.Save(@"C:\Users\Nico\Desktop\output.png");
         }
 
-        public static byte[] ImageToByte(Image img)
-        {
-            ImageConverter converter = new ImageConverter();
-            return (byte[])converter.ConvertTo(img, typeof(byte[]));
-        }
-
         public static void readLSB(string filename)
         {
-            Bitmap bmp = new Bitmap(@"C:\Users\Nico\Desktop\output.png");
-            Color pixel;
-            string shades = "", final = "";
-            for (int i = 0; i < bmp.Width; i++)
+            
+            Bitmap bmp = new Bitmap(filename);
+            byte[] message = new byte[bmp.Height * bmp.Width];
+
+            // Lock the bitmap's bits.  
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
+
+            // Get the address of the first line.
+            IntPtr ptr = bmpData.Scan0;
+
+            int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
+            byte[] rgbValues = new byte[bytes];
+
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+
+            int byteCount = -1;
+            for (int i = 0; i < rgbValues.Length; i++)
             {
-                for (int j = 0; j < bmp.Height; j++)
-                {
-                    pixel = bmp.GetPixel(i, j);
-                    shades += "" + (pixel.R % 2) + "" + (pixel.G % 2) + "" + (pixel.B % 2);
-                }
+                if (i % 8 == 0)
+                    byteCount++;
+
+                // Add the LSB to bitArray
+                message[byteCount] += (byte)(rgbValues[i] & (1 << 7));
             }
 
-            byte[] bytes = GetBytesFromBinaryString(shades);
-            foreach(byte b in bytes)
-            {
-                final += Convert.ToChar(b);
-            }
-            File.Delete(@"C:\Users\Nico\Desktop\shades.txt");
-            File.WriteAllText(@"C:\Users\Nico\Desktop\shades.txt", final, Encoding.GetEncoding("UTF-8"));
-            MessageBox.Show("done");
+            // Copy the RGB values back to the bitmap & unlock
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+            bmp.UnlockBits(bmpData);
+
+            // Show the message
+            MessageBox.Show(Encoding.Default.GetString(message));
         }
-
-        public static Byte[] GetBytesFromBinaryString(String binary)
-        {
-            var list = new List<Byte>();
-
-            for (int i = 0; i < binary.Length; i += 8)
-            {
-                String t = binary.Substring(i, 8);
-
-                list.Add(Convert.ToByte(t, 2));
-            }
-
-            return list.ToArray();
-        }
-
     }
 }
